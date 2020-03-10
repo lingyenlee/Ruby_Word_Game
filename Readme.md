aeb8e21715c94cd0eadd03a3d71a3e19a58178d1
lingyenlee <lingyenlee@gmail.com>
2020-03-10 11:42:16 +1100

    first commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+Word Game
+
+-The goal of the game is to score as many points from forming words from a 10-letter word to reach the chosen difficulty level
+-Game starts with a 10-letter word that has been randomly shuffled
+-User then forms valid words from the word
+-Show the score board that has points for each letter
+-User has 5 inputs and score for each word formed is returned after each input
+-There are 3 levels with increasing points - easy (30pts), medium (50pts) or difficult(80pts)
+-If the user reached the points, the user wins, otherwise the user loses
+-After the round, ask if user wants to play again, if not, quits game
---
+++ test.rb
@@ -0,0 +1,44 @@
+# word = "affordable"
+# array = word.split("")
+
+# puts array.to_s
+# shuffle = array.shuffle
+# puts shuffle.to_s
+
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+# require 'pry'
+# Fetch and parse HTML document
+doc = Nokogiri::HTML(open('https://wordfinder.yourdictionary.com/letter-words/10/'))
+
+words = []
+
+table = doc.at("table")
+table.search('td.table-cell-word').each do |i|
+    words.push(i.content)
+end
+
+puts words
+# binding.pry
+# puts doc
+
+# puts "### Search for nodes by css"
+# doc.css('nav ul.menu li a', 'article h2').each do |link|
+#   puts link.content
+# end
+
+# puts "### Search for nodes by xpath"
+# doc.xpath('//nav//ul//li/a', '//article//h2').each do |link|
+#   puts link.content
+# end
+
+# puts "### Or mix and match."
+# rows = doc.css("td.table-cell-word")
+# puts rows
+# doc.search('td.table-cell-word').each do |link|
+# #     words = []
+# #   words.push(link.content)
+# puts link.content
+# #   puts words.to_s
+# end
