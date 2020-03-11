b76fe9edbcd1222441166a22cae2e17f17ae8256
lingyenlee <lingyenlee@gmail.com>
2020-03-11 17:07:39 +1100

    fix error message

--- word_game.rb
+++ word_game.rb
@@ -34,10 +34,6 @@ def make_hash(word)
     return hash
 end
 
-def get_err(message)
-    return message
-end
-
 # find word in dictionary
 def get_word(word)
     error_message = []
@@ -56,40 +52,39 @@ def get_word(word)
         # parse Json to Hash
         my_resp = JSON.parse(resp)
 
+        # return my_resp['id']
         # if my_resp['id']
         if my_resp['id'] == word
-            return "Your word is valid."
+            return true
         else 
             return false
         end
     end
 end
 
-def check_word(shuffle, input)
+def check_letter(shuffle, input)
 
-    error_message = []
-    is_word = get_word(input)
-    hash_shuffle = make_hash(shuffle)
-    hash_input = make_hash(input)
+    # make hash and store letter count
+    shuffle_hash = make_hash(shuffle)
+    input_hash = make_hash(input)
+    diff = []
 
-    if !is_word
-        error_message.push("Error - Invalid word. Try again.")
-    end
-
-    hash_input.each do |k, v|
-        if !hash_shuffle.has_key?(k) 
-            error_message.push("Error - letter(s) not found in word. Try again")
-        elsif v > hash_shuffle[k]
-            error_message.push("Error - repeated letter(s). Try again")
+    input_hash.each do |k, v|
+        if shuffle_hash.has_key?(k) && v > shuffle_hash[k]
+            diff.push(k)
+        elsif !shuffle_hash.has_key?(k)
+            diff.push(k)
         else
             next
         end
     end
-    return error_message
+    return diff
 end
 
-puts check_word("consolidate", "congratulate")
-
+# def get_total(acc, current)
+#     total = acc + current
+#     return total
+# end
 
 # count word score
 def get_word_score(word_input)
@@ -101,48 +96,14 @@ def get_word_score(word_input)
         "s" => 1, "t" => 1, "u" => 1, "v" => 4, "w" => 4, "x" => 8, "y" => 4, "z" => 10 }
 
     # initialize total score and input number
-    total_score = 0
     word_score = 0
-
-    # word_input = get_word_input
-    
+    # total_score = 0
+ 
     word_input.chars {|x| word_score += score_key[x]}
     puts "Your word score is #{word_score}."
-
-    # accumulate and display score score
-    total_score += word_score
-    puts "Your total score is #{total_score}."
-
-    
-    # input_num = 1  
-    
-    # while input_num < 4 
-        
-    #     # ask for word input and convert word to upper case
-        
-    #     word_input = get_response("Make a word: ")
-    
-    #     # check word
-    #     puts is_word(word_input)
-       
-    #     puts check_word(word_input)
-       
-    #     # initialize word_score and map through each char and add points
-    #     word_score = 0
-    #     word_input.chars {|x| word_score += score_key[x]}
-    #     puts "Your word score is #{word_score}."
-
-    #     # accumulate and display score score
-    #     total_score += word_score
-    #     puts "Your total score is #{total_score}."
-    #     input_num += 1
-    # end
-
-    # return total_score
+    return word_score
   
 end
-
-# shuffle_word = shuffle_word("consolidation")
 
 # get response from input
 def get_response(prompt_text)
@@ -164,62 +125,90 @@ end
 
 def play_game
     
-    # get name
-    get_name = "What is your name? "
-    name = get_response(get_name)
-    
-    # get level of difficulty
-    get_level = "Hello #{name}, choose play level (points to win): 1 for Easy (10pts), 2 for Medium (20pts), 3 for Difficult (40pts) "
-    while play_level = get_response(get_level).to_i
-        case play_level
-        when 1
-            score_threshold = 10
-            break
-        when 2
-            score_threshold = 20
-            break
-        when 3
-            score_threshold = 40
-            break
-        else
-            puts "You entry is not valid. Please choose again"
+    while true
+        # get name
+        get_name = "What is your name? "
+        name = get_response(get_name)
+        
+        # get level of difficulty
+        get_level = "Hello #{name}, choose play level (points to win): 1 for Easy (10pts), 2 for Medium (20pts), 3 for Difficult (40pts) "
+        while play_level = get_response(get_level).to_i
+            case play_level
+            when 1
+                score_threshold = 10
+                break
+            when 2
+                score_threshold = 20
+                break
+            when 3
+                score_threshold = 40
+                break
+            else
+                puts "You entry is not valid. Please choose again"
+            end
+        end    
+
+        # present shuffle word
+        word_to_play = "consolidate"
+        shuffled_word = shuffle_word(word_to_play)
+        arr = []
+        word_hash = {}
+        i = 1
+        total_score = 0
+        while i < 4
+            # display shuffled word
+            puts shuffled_word
+
+            # get word input and save to an array and make a hash to keep trash
+            word_input = get_word_input(i)
+            arr.push(word_input)
+            arr.each do |i|
+                if word_hash.has_key?(i)
+                    word_hash[i] += 1
+                else
+                    word_hash[i] = 1
+                end
+            end
+        
+            # get value of checkings
+            check_word = get_word(word_input)
+            check_letter = check_letter(shuffled_word, word_input)
+        
+
+            if check_word && check_letter.length > 0
+                puts "Letter(s) not found or too many. Try again."
+                word_input 
+                # i -= 1
+                next
+            elsif !check_word
+                puts "Invalid word. Try again."
+                word_input 
+                # i -= 1
+                next
+            elsif check_letter.length > 0 
+                puts "Letter(s) not found or too many. Try again."
+                word_input 
+                # i -= 1
+                next
+            elsif word_hash[word_input] > 1
+                puts "Word has been used. Try again."
+                word_input 
+                next
+            else
+                puts "Valid word."
+            end
+            i += 1
+            word_score = get_word_score(word_input)
+            total_score += word_score
+            puts "Total score is #{total_score}."
         end
-    end    
-
-
-    # present shuffle word
-    word_to_play = "consolidate"
-    puts shuffled_word = shuffle_word(word_to_play)
-    i = 1
-    while i < 4
-        word_input = get_word_input(i)
-
-
-        puts rep1 = is_word(word_input)
-        puts rep2 = check_word(word_to_play, word_input)
-       
-
-        # if rep1.include?("Error") || rep2.include?("Error")# check if invlid
-        #     # puts shuffled_word
-        #     word_input = get_word_input(i)
-        # else
-        #     next
-        # end
-        i += 1
+        puts results(total_score, score_threshold)
+        play_again = get_response("Do you want to play again (y/n).")
+        
     end
-    
-    # total_score = get_score
-    # puts results(total_score, score_threshold)
+
 end
 
 
 # word_input
-# play_game
-
-
-
-
-
-
-
-
+play_game
